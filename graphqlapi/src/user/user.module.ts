4620a26b23b3f39f3255109081e279da1c365740
shameem <mohammed.shameem@uk.ey.com>
2020-01-22 13:52:23 +0000

    Revert "initial commit"
    This reverts commit d2852710bfb69dc5d8f1232314dcdb8789ac0e7c.

--- graphqlapi/src/user/user.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UserResolver } from './user.resolver';
-import { UserClientService } from './user-client.service';
-
-@Module({
-  providers: [UserResolver, UserClientService]
-})
-export class UserModule {}
--- user-service/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UserController } from './user/user.controller';
-
-@Module({
-  imports: [],
-  controllers: [UserController],
-  providers: [],
-})
-export class AppModule {}
